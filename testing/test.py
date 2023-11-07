be6122a188d8a7f9aefdc8d07f58777176170e5b
Tervicke <ameywpatil@gmail.com>
2023-11-07 18:50:22 +0530

    added testing units

---
+++ testing/test.py
@@ -0,0 +1,17 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from flask import jsonify
+import sqlite3 as db
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    conn = db.connect('dat.db' , check_same_thread=False)
+    cur = conn.cursor()
+    cur.execute("Select * from data")
+    return jsonify(cur.fetchall());
+
+if __name__ =='__main__':  
+    app.run(debug = True)
